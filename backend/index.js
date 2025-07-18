dc578c037113ad96208de99061011d5f9c607a02
abhiyendru <rahul.r0644@gmail.com>
2025-07-18 20:02:30 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -9,10 +9,23 @@ import userRouter from "./routes/user.routes.js"
 import geminiResponse from "./gemini.js"
 
 
-app.use(cors({
-    origin: "*",
+const allowedOrigins = [
+    "https://ai-assistant-uuve.onrender.com",
+    "https://ai-assistant673.vercel.app",
+    "http://localhost:5173"
+  ];
+  
+  app.use(cors({
+    origin: function(origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error('Not allowed by CORS'), false);
+      }
+    },
     credentials: true
-}))
+  }))
   
 const port=process.env.PORT
 app.use(express.json())
